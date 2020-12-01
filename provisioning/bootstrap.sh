7d11c6ce0219d3cbfd81b57e4b96b87ba44298d9
Patrick Bateman <batemapf@gmail.com>
2020-11-30 23:50:28 -0500

    clean up bootstrap script

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 set -x;
 
+# Load secrets as environment variables
+source ./load_secrets.sh;
+
 # Install OS dependencies
 sudo apt update;
 sudo apt-get -y install apache2 apache2-utils libexpat1 ssl-cert python3 \
@@ -15,6 +18,7 @@ cd launchpad;
 bash ./setup.sh;
 
 # Configure Apache and reload server
+sudo chown ubuntu:ubuntu /etc/apache2/apache2.conf;
 echo "
 WSGIScriptAlias / /home/ubuntu/launchpad/launchpad/wsgi.py
 WSGIPythonHome /home/ubuntu/launchpad/launchpadenv
@@ -32,13 +36,11 @@ Require all granted
 # Require all granted
 # </Directory>
 " >> /etc/apache2/apache2.conf;
+sudo chown root:root /etc/apache2/apache2.conf;
 sudo systemctl reload apache2;
 
-# Load secrets as environment variables
-source ../load_secrets.sh;
+# Make all files in home dir owned by user
+sudo chown -R ubuntu:ubuntu /home/ubuntu
 
 # Poll for new commits from GitHub
 nohup bash ./poll_for_commits.sh &
-
-# Make all files in home dir owned by user
-sudo chown -R ubuntu:ubuntu /home/ubuntu/
